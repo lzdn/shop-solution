4c76c183bc4fc15e7c41e9412c3f8a09e5829b69
zhanglin <415656544@qq.com>
2018-05-30 01:00:57 +0800

    1

---
+++ shop-manage/src/main/java/com/shop/App.java
@@ -0,0 +1,44 @@
+package com.shop;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@Configuration
+@EnableAutoConfiguration
+public class App extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
+
+	// 增加此代码是因为 com.xuexi.conf.LogAspect
+	// 会用到RequestContextHolder，不然会报空指针。springMVC则是通过配置文件配置
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(1024L * 20L);
+		factory.setFileSizeThreshold(0);
+		return factory.createMultipartConfig();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+}
