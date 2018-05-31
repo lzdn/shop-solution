52f767df36a3312d0073510c6bcc53c1a2baaff0
zhanglin <415656544@qq.com>
2018-06-01 01:34:02 +0800

    1

---
+++ shop-service/src/main/java/com/shop/service/ITest.java
@@ -0,0 +1,11 @@
+package com.shop.service;
+
+import com.shop.domain.manage.Test;
+import com.shop.domain.product.Test2;
+
+public interface ITest {
+
+	void addTest(Test record);
+
+	void addTest2(Test2 record);
+}
