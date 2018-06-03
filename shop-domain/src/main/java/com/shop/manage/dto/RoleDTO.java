c1bf8632890602b864c09beb752fc0a5d07352c1
zhanglin <415656544@qq.com>
2018-06-03 19:49:39 +0800

    1

---
+++ shop-domain/src/main/java/com/shop/manage/dto/RoleDTO.java
@@ -0,0 +1,16 @@
+package com.shop.manage.dto;
+
+import com.shop.manage.domain.Role;
+
+/**
+* @date 20180602
+* @author 张林
+* @description 角色
+* @gitHub:https://github.com/lzdn
+*/
+public class RoleDTO extends Role{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
---
+++ shop-domain/src/main/java/com/shop/manage/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.shop.manage.dto;
+
+import com.shop.manage.domain.User;
+
+/**
+* @date 20180602
+* @author 张林
+* @description 用户
+* @gitHub:https://github.com/lzdn
+*/
+public class UserDTO extends User{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
