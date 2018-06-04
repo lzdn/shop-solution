3b318e1dd9f91568d4e0861a022da26929ad61ae
zhanglin <zhanglin@htke.com>
2018-06-04 18:26:52 +0800

    1

--- shop-manage/src/main/java/com/shop/utils/MD5Util.java
+++ shop-manage/src/main/java/com/shop/utils/MD5Util.java
@@ -1,10 +1,63 @@
 package com.shop.utils;
 
 import java.security.MessageDigest;
+import java.util.Random;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class MD5Util {
 
 	public static final String SALT = "@ad902s^$";
+
+	private static final String BASE_RANDOM = "zxcvbnmlkjhgfdsaqwertyuiopQWERTYUIOPASDFGHJKLZXCVBNM1234567890@^$";
+
+	private static final int len = 9;
+
+	public static String getRandomSalt() {
+		// 定义一个字符串（A-Z，a-z，0-9）即62位；
+		// 由Random生成随机数
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+		// 长度为几就循环几次
+		for (int i = 0; i < len; ++i) {
+			// 产生0-61的数字
+			int number = random.nextInt(62);
+			// 将产生的数字通过length次承载到sb中
+			sb.append(BASE_RANDOM.charAt(number));
+		}
+		// 将承载的字符转换成字符串
+		return sb.toString();
+	}
+
+	public static String encode(String password, String salt) {
+
+		if (StringUtils.isEmpty(salt))
+			return encode(password);
+
+		password = password + salt;
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		char[] charArray = password.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++)
+			byteArray[i] = (byte) charArray[i];
+		byte[] md5Bytes = md5.digest(byteArray);
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16) {
+				hexValue.append("0");
+			}
+
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+	}
 
 	public static String encode(String password) {
 		password = password + SALT;
@@ -33,8 +86,7 @@ public class MD5Util {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(MD5Util.encode("123"));
-
+		System.out.println(MD5Util.encode("123", MD5Util.getRandomSalt()));
 	}
 
 }
--- shop-manage/target/classes/com/shop/conf/WebSecurityConfig.java
+++ shop-manage/target/classes/com/shop/conf/WebSecurityConfig.java
@@ -19,66 +19,47 @@ import com.shop.security.CustomUserDetailsService;
 import com.shop.security.LoginSuccessHandler;
 import com.shop.utils.MD5Util;
 
-
- 
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private CustomSecurityFilter customSecurityFilter;
-	
+
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
-	
+
 	@Autowired
 	private CustomAccessDeniedHandler customAccessDeniedHandler;
 
-	/*@Autowired
-	private WebProperties webProperties;*/
-	
+
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
+
 	@Bean
 	public AuthenticationManager customAuthenticationManager() throws Exception {
 		return authenticationManager();
 	}
- 
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 
-		  http.csrf().disable() 
-	        .authorizeRequests()  
-	        .antMatchers("/static/**").permitAll()
-	        .antMatchers("/register").permitAll()
-	        .antMatchers("/druid/**").permitAll()
-	        .anyRequest().authenticated()  
-	        .and()  
-	        .formLogin()  
-	        .loginPage("/login")      
-	  //      .failureUrl("/login?error")
-	        .successHandler(loginSuccessHandler())//code3
-//	        .defaultSuccessUrl("/home")  defaultSuccessUrl 和 successHandler只能用一个，否则 defaultSuccessUrl 会覆盖 successHandler
-	        .permitAll()
-	        .and()  
-	        .logout()  
-	        .logoutSuccessUrl("/login")  
-	        .permitAll()  
-	        .invalidateHttpSession(true)  
-	        .and()  
-	        .rememberMe()  
-	        .tokenValiditySeconds(1209600);  
-		 http.headers().frameOptions().disable();//解决页面iframe的问题 X-Frame-Options
-		 http.exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
-		 http.addFilterAfter(customSecurityFilter, FilterSecurityInterceptor.class);
-		 
-		 //在正确的位置添加我们自定义的过滤器  
-		
-	 
+
+		http.csrf().disable().authorizeRequests().antMatchers("/static/**").permitAll().antMatchers("/register")
+				.permitAll().antMatchers("/druid/**").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login")
+				// .failureUrl("/login?error")
+				.successHandler(loginSuccessHandler())// code3
+				// .defaultSuccessUrl("/home") defaultSuccessUrl 和 successHandler只能用一个，否则
+				// defaultSuccessUrl 会覆盖 successHandler
+				.permitAll().and().logout().logoutSuccessUrl("/login").permitAll().invalidateHttpSession(true).and()
+				.rememberMe().tokenValiditySeconds(1209600);
+		http.headers().frameOptions().disable();// 解决页面iframe的问题 X-Frame-Options
+		http.exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
+		http.addFilterAfter(customSecurityFilter, FilterSecurityInterceptor.class);
+		// 在正确的位置添加我们自定义的过滤器
+
 	}
 
 	@Override
@@ -91,7 +72,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		// 指定密码加密所使用的加密器为passwordEncoder()
 		// 需要将密码加密后写入数据库
-		//auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+		// auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
 		auth.userDetailsService(customUserDetailsService).passwordEncoder(new PasswordEncoder() {
 			@Override
 			public String encode(CharSequence rawPassword) {
