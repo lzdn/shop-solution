5c3fdf6a19c81a334e346b5ede7e5d91a4e5332b
zhanglin <zhanglin@htke.com>
2018-07-06 22:43:34 +0800

    1

--- shop-manage/src/main/java/com/shop/App.java
+++ shop-manage/src/main/java/com/shop/App.java
@@ -16,7 +16,7 @@ import org.springframework.web.context.request.RequestContextListener;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @Configuration
 @ServletComponentScan
-@ComponentScan(basePackages = { "com.shop"/*,"com.data"*/ })
+@ComponentScan(basePackages = { "com.shop"})
 @EnableAutoConfiguration
 public class App extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
 
