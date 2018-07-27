d4fc0a5148d622fd73e56a1cfb2aa7012d2e7745
zhanglin <zhanglin@htke.com>
2018-07-27 21:26:05 +0800

    1

---
+++ shop-resource/src/main/java/com/shop/App.java
@@ -0,0 +1,23 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author: zhanglin
+ * @program: shop-solution
+ * @description: 资源管理
+ * @create: 2018-07-27 15:59
+ * @github:https://github.com/lzdn
+ * @oschina:https://my.oschina.net/lzdn
+ **/
+@SpringBootApplication
+@EnableAutoConfiguration
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
